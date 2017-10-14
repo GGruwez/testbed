390634d12cb63902f6beddb10167173470174560
GGruwez <gilles.gruwez@yahoo.com>
2017-10-14 20:40:06 +0200

    aircraft

---
+++ Testbed/src/mygame/Aircraft.java
@@ -0,0 +1,35 @@
+package mygame;
+
+import java.util.ArrayList;
+
+public class Aircraft {
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param xVelocity
+	 * @param yVelocity
+	 * @param zVelocity
+	 * @param mass
+	 * @param thrust
+	 * @param leftWingInclination
+	 * @param rightWingInclination
+	 * @param horStabInclination
+	 * @param verStabInclination
+	 */	
+	public Aircraft(float x, float y, float z, float xVelocity, float yVelocity, float zVelocity,
+		float mass, float thrust, float leftWingInclination,float rightWingInclination,
+		float horStabInclination, float verStabInclination ){
+			
+	}
+		
+	public ArrayList getCoordinates(){
+		
+	}
+	
+	
+		
+		
+}
+
---
+++ Testbed/src/mygame/Forces.java
@@ -0,0 +1,36 @@
+package mygame;
+import java.util.ArrayList;
+
+public class Forces {
+	
+	
+	public ArrayList getTotalLift(){
+		return this.getLeftWingLift() + this.getRightWingLift() 
+		+ this.getHorizontalStabilizerLift() + this.getVerticalStabilizerLift();
+				
+	}
+	
+	public ArrayList getLeftWingLift(){
+		
+	}
+	
+	public ArrayList getRightWingLift(){
+		
+	}
+	
+	public ArrayList getHorizontalStabilizerLift(){
+		
+	}
+	
+	public ArrayList getVerticalStabilizerLift(){
+		
+	}
+	
+	
+	
+	public ArrayList getMass(){
+		
+	}
+	
+	
+}
