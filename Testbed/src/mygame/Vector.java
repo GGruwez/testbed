2b390bc97edd2fc748fe1f044f305c90345c2408
GGruwez <gilles.gruwez@yahoo.com>
2017-10-15 18:38:55 +0200

    added dotproduct, crossproduct

--- Testbed/src/mygame/Vector.java
+++ Testbed/src/mygame/Vector.java
@@ -40,11 +40,26 @@ public class Vector {
 	// transformatiematrixen, daar kan ik echt nie mee werken
 	public Vector transform(double heading, double pitch, double roll ){
 		
-		
-		
 		return new Vector(0,0,0);
-		
-		
+	}
+	
+	/**
+	 * A.B
+	 * @param other
+	 * @return
+	 */
+	public double dotProduct(Vector other){
+		return this.x*other.x + this.y*other.y + this.z*other.z;
+	}
+	
+	/**
+	 * AxB
+	 * @param other
+	 * @return
+	 */
+	public Vector crossProduct(Vector other){
+		return new Vector(this.y*other.z - other.y*this.z,
+				-this.x*other.z+other.x*this.z, this.x*other.y - other.x*this.y);
 	}
 	
 }	
