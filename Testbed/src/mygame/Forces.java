7753b90ce81e734f6a6669dc4471ce67948f0ca3
GGruwez <gilles.gruwez@yahoo.com>
2017-10-15 15:04:01 +0200

    Heb wat gewerkt aan forces, airfoil class toegevoegd. Nog niets definitief, gewoon enkele ideetjes voor morgen voorbereid. Zal hier waarschijnlijk vanavond en morgenochtend nog aan verder werken.

--- Testbed/src/mygame/Forces.java
+++ Testbed/src/mygame/Forces.java
@@ -1,34 +1,74 @@
 package mygame;
-import java.util.ArrayList;
+import java.lang.Object;
+import java.Vector;
+import java.math;
 
 public class Forces {
 	
+	private double x;
+	private double y;
+	private double z;
 	
-	public ArrayList getTotalLift(){
+	protected Forces(double x, double y, double z){
+		this.setX(x);
+		this.setY(y);
+		this.setZ(z);
+	}
+	
+	
+	public void setX(double x){
+		this.x = x;
+	}
+	
+	
+	public void setY(double y){
+		this.y = y;
+	}
+	
+	
+	public void setZ(double z){
+		this.z = z;
+	}
+	
+	public Vector axisWing = Vector3D(1,0,0);
+	public Vector axisHorizontalStabilizer = Vector(1,0,0);
+	public Vector axisVerticalStabilizer = Vector(0,1,0);
+	
+	public Vector attackLeftWing = Vector(0, sin(this.leftWingInclination), -cos(this.leftWingInclination));
+	public Vector attackRightWing = Vector(0, sin(this.rightWingInclination), -cos(this.rightWingInclination));
+	public Vector attackHorizontalStabilizer = Vector(0, sin(horStabInclination), -cos(horStabInclination));
+	public Vector attackVerticalStabilizer = Vector(-sin(verStabInclination), 0, -cos(verStabInclination));
+	
+	public Vector getAttackVector(){
+		if 
+	}
+			
+	
+	public Vector getTotalLift(){
 		return this.getLeftWingLift() + this.getRightWingLift() 
 		+ this.getHorizontalStabilizerLift() + this.getVerticalStabilizerLift();
 				
 	}
 	
-	public ArrayList getLeftWingLift(){
+	public Vector getLeftWingLift(){
 		
 	}
 	
-	public ArrayList getRightWingLift(){
+	public Vector getRightWingLift(){
 		
 	}
 	
-	public ArrayList getHorizontalStabilizerLift(){
+	public Vector getHorizontalStabilizerLift(){
 		
 	}
 	
-	public ArrayList getVerticalStabilizerLift(){
+	public Vector getVerticalStabilizerLift(){
 		
 	}
 	
 	
 	
-	public ArrayList getMass(){
+	public Vector getMass(){
 		
 	}
 	
