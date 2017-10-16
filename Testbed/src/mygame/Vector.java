11cac4ce7a64fe4878115f7d2a9474e8ff49b99c
GGruwez <gilles.gruwez@yahoo.com>
2017-10-16 16:15:38 +0200

    transformation

--- Testbed/src/mygame/Vector.java
+++ Testbed/src/mygame/Vector.java
@@ -50,11 +50,21 @@ public class Vector {
 	}
 	
 	
-	// transformatiematrixen, daar kan ik echt nie mee werken
-	// ik kijk daar wel eens naar als ik mn boek van mechanica 3 vind, staat daar allemaal in
+	/**
+	 * 
+	 * @param heading - heading van de aircraft tov wereld
+	 * @param pitch - pitch van de aircraft tov wereld
+	 * @param roll - roll van de aircraft tov wereld
+	 * @return
+	 */
 	public Vector transform(double heading, double pitch, double roll ){
+		return new Vector(this.x*(Math.cos(heading)*Math.cos(pitch))+this.y*(Math.cos(heading)*Math.sin(pitch)*Math.sin(roll)-Math.sin(heading)*Math.cos(roll))+this.z*(Math.cos(heading)*Math.sin(pitch)*Math.cos(roll)+Math.sin(heading)*Math.sin(roll)),
+				this.x*(Math.sin(heading)*Math.cos(pitch))+this.y*(Math.sin(heading)*Math.sin(pitch)*Math.sin(roll)+Math.cos(heading)*Math.cos(roll))+this.z*(Math.sin(heading)*Math.sin(pitch)*Math.cos(roll)-Math.cos(heading)*Math.sin(roll)),
+				this.x*(-Math.sin(pitch))+this.y*(Math.cos(pitch)*Math.sin(roll))+this.z*(Math.cos(pitch)*Math.cos(roll)));
+				
+				
 		
-		return new Vector(0,0,0);
+		
 	}
 	
 	/**
