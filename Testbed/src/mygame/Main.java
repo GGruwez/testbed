1a05a5ffeaa672f44308c766402a566edc73afec
daan Lenaerts <me@daanlenaerts.com>
2017-11-20 18:51:59 +0100

    Working on UI changes

--- Testbed/src/mygame/Main.java
+++ Testbed/src/mygame/Main.java
@@ -4,6 +4,8 @@ import com.jme3.system.AppSettings;
 import com.jme3.system.JmeCanvasContext;
 import com.jme3.system.JmeContext;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 
 public class Main {
@@ -34,13 +36,35 @@ public class Main {
                 JFrame window = new JFrame("Testbed");
                 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                JPanel panel = new JPanel(new FlowLayout()); // a panel
-                // add all your Swing components ...
-                panel.add(new JButton("Button"));
+                JPanel panel = new JPanel(new BorderLayout());
 
-                // add the JME canvas
-                panel.add(ctx.getCanvas());
+                JTabbedPane tabbedPane = new JTabbedPane();
 
+                JComponent panel1 = new JPanel();
+                Canvas c = ctx.getCanvas();
+                panel1.add(new Panel());
+                panel1.add(c);
+
+                tabbedPane.addTab("Regular view", null, panel1);
+
+                JComponent panel2 = new JPanel();
+                panel2.add(new JButton("Button"));
+                tabbedPane.addTab("Configuration", null, panel2);
+
+
+                tabbedPane.addChangeListener(new ChangeListener() {
+                    @Override
+                    public void stateChanged(ChangeEvent e) {
+                        int currentIndex = tabbedPane.getSelectedIndex();
+                        if (currentIndex == 1){
+                            panel1.remove(c);
+                        }else if(currentIndex == 0){
+                            panel1.add(c);
+                        }
+                    }
+                });
+
+                panel.add(tabbedPane);
                 window.add(panel);
                 window.pack();
                 window.setVisible(true);
