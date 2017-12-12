c5f244994668cfb77d5adba45ea25523adcc9be3
Simon Smeets <simon-smeets@hotmail.be>
2017-12-12 12:14:12 +0100

    fixed diff in image passing

--- Testbed/src/mygame/Aircraft.java
+++ Testbed/src/mygame/Aircraft.java
@@ -269,7 +269,7 @@ public class Aircraft extends Node {
     	setRoll(getRoll() + angularVel.getZ()*time);
     	setHeading(getHeading() + angularVel.getY()*time);
     	
-    	System.out.println()
+    	System.out.println();
 
         //Vector totalM = getForce().getTotalMoment().applyInertiaTensor(this.getForce().getInverseInertia());
         //totalM.printVector("totalm ");
@@ -341,7 +341,7 @@ public class Aircraft extends Node {
         this.getForce().setThrust(autopilotOutputs.getThrust());
         this.setLeftWingInclination(autopilotOutputs.getLeftWingInclination());
         this.setRightWingInclination(autopilotOutputs.getRightWingInclination());
-        this.setHorStabInclination(-autopilotOutputs.getHorStabInclination());
+        this.setHorStabInclination(autopilotOutputs.getHorStabInclination());
         this.setVerStabInclination(autopilotOutputs.getVerStabInclination());
     }
     
