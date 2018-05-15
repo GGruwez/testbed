e3aeb3d6519497cb91cda4a7510ddec0223c51cf
crouxderoo <ruben.croux@hotmail.com>
2018-05-15 14:20:09 +0200

    GUI class

---
+++ Testbed/src/mygame/PackageGUI.java
@@ -0,0 +1,17 @@
+
+package mygame;
+
+import javax.swing.JPanel;
+
+
+public class PackageGUI extends JPanel {
+    
+    private MainSwingCanvas canvas;
+    
+    public PackageGUI(MainSwingCanvas canvas) {
+        this.canvas = canvas;
+    }
+    
+    
+    
+}
